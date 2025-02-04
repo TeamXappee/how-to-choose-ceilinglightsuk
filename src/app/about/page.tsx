6c61357029805e4f9d48bcdb2eb52942110e9355
TeamXappee <xappeesoftware@gmail.com>
2025-02-03 22:32:40 -1000

    project

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,6 @@
-// import { Footer } from "@/components/Footer";
-// import { Header } from "@/components/Header";
-// import { config } from "@/config";
-// import { signOgImageUrl } from "@/lib/og-image";
-// import Markdown from "react-markdown";
+import { Footer } from "@/components/Footer";
+import { Header } from "@/components/Header";
+import { config } from "@/config";
 
 // const content = `# About Me
 
@@ -26,34 +24,34 @@
 
 // Samantha`;
 
-// export async function generateMetadata() {
-//   return {
-//     title: "About Me",
-//     description: "Learn more about Samantha and her travel adventures",
-//     openGraph: {
-//       title: "About Me",
-//       description: "Learn more about Samantha and her travel adventures",
-//       images: [
-//         signOgImageUrl({
-//           title: "Samantha",
-//           label: "About Me",
-//           brand: config.blog.name,
-//         }),
-//       ],
-//     },
-//   };
-// }
+export async function generateMetadata() {
+  return {
+    title: "About Me",
+    description: "Learn more about Samantha and her travel adventures",
+    openGraph: {
+      title: "About Me",
+      description: "Learn more about Samantha and her travel adventures",
+    //   images: [
+    //     signOgImageUrl({
+    //       title: "Samantha",
+    //       label: "About Me",
+    //       brand: config.blog.name,
+    //     }),
+    //   ],
+    },
+  };
+}
 
-// const Page = async () => {
-//   return (
-//     <div className="container mx-auto px-5">
-//       <Header />
-//       <div className="prose lg:prose-lg dark:prose-invert m-auto mt-20 mb-10 blog-content">
-//         <Markdown>{content}</Markdown>
-//       </div>
-//       <Footer />
-//     </div>
-//   );
-// };
+const Page = async () => {
+  return (
+    <div className="container mx-auto px-5">
+      <Header />
+      {/* <div className="prose lg:prose-lg dark:prose-invert m-auto mt-20 mb-10 blog-content">
+        <Markdown>{content}</Markdown>
+      </div> */}
+      <Footer />
+    </div>
+  );
+};
 
-// export default Page;
+export default Page;
